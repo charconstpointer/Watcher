1bac7d1f64d51bac8d99a77eef654a121788dbee
attwistingnether@gmail.com <Sha512 mate>
2019-02-24 19:38:14 +0100

    mapping

---
+++ Watcher/Mapping/UserMapping.cs
@@ -0,0 +1,15 @@
+using TableDependency.SqlClient.Base;
+
+namespace Watcher.Mapping
+{
+    public class UserMapping
+    {
+        public static ModelToTableMapper<User> GetMapper()
+        {
+            var mapper = new ModelToTableMapper<User>();
+            mapper.AddMapping(u => u.Id, "Id");
+            mapper.AddMapping(u => u.Username, "Username");
+            return mapper;
+        }
+    }
+}
--- Watcher/Program.cs
+++ Watcher/Program.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using TableDependency.SqlClient;
-using TableDependency.SqlClient.Base;
 using TableDependency.SqlClient.Base.EventArgs;
-using Watcher.Domain.Models;
-using Watcher.Notifications;
+using Watcher.Mapping;
 
 namespace Watcher
 {
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+
     internal static class Program
     {
         private static void OnChange(object sender, RecordChangedEventArgs<User> e)
         {
+            Notify(e.Entity.Username).Start();
             Console.WriteLine("Id " + e.Entity.Id + " Name " + e.Entity.Username);
-            Notification.Notify(e.Entity.Username).Start();
         }
 
+        private static async Task Notify(string entity)
+        {
+            using (var client = new HttpClient())
+            {
+                var message = entity + " added to db";
+                var res = await client.PostAsync("https://localhost:44303/api/events",
+                    message.AsJson()
+                );
+                Console.WriteLine(res.ReasonPhrase);
+            }
+        }
 
         private static void Main(string[] args)
         {
@@ -25,13 +42,11 @@ namespace Watcher
                 .AddJsonFile("appsettings.json");
 
             var configuration = builder.Build();
-            var cs = configuration.GetSection("DbConnections").GetSection("Default").Value;
-            var mapper = new ModelToTableMapper<User>();
-            mapper.AddMapping(u => u.Id, "Id");
-            mapper.AddMapping(u => u.Username, "Username");
+            var cs = configuration.GetSection("DbConnections").GetSection("Backup").Value;
+
 
             using (var dependency = new SqlTableDependency<User>
-                (cs, "Users", mapper: mapper, executeUserPermissionCheck: false))
+                (cs, "Users", mapper: UserMapping.GetMapper(), executeUserPermissionCheck: false))
             {
                 dependency.OnChanged += OnChange;
                 dependency.Start();
